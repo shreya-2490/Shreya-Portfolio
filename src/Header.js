04447237763cd983b94b9dbc2c912d0fedec6548
shreya072001 <gsmile.india201@gmail.com>
2023-08-08 23:14:52 +0530

    changed

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ import { saveAs } from 'file-saver';
 function Header() {
   const handleDownload = () => {
     // Assuming your CV file is located at the given URL
-    const cvUrl = 'https://drive.google.com/file/d/1dVr2JgAvLwDoZ3nIk2eFGYaDv6Nld-xo/view?usp=sharing';
+    const cvUrl = 'https://drive.google.com/file/d/1_4OgHV9mr7cDzDtRUXWaXNExtVkj9-t7/view?usp=sharing';
 
     // Trigger the download using FileSaver.js
     saveAs(cvUrl, 'CV.pdf');
